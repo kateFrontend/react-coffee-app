b03b13491963ed2dc887c888d3623c45e6a291d1
kateFrontend <ecdanilova@gmail.com>
2022-12-01 16:54:15 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <nav>
+        <Link to="/home" className="link">
+          Home
+        </Link>
+        <Link to="/about" className="link">
+          About Us
+        </Link>
+        <Link to="/contact" className="link">
+          Contact Us
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />/
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
