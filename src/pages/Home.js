e0a3d7f79fba2a5582829df203c7b520bd74eb9d
kateFrontend <ecdanilova@gmail.com>
2022-12-07 21:48:07 -0500

    about component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import About from "../components/About";
 
 function Home() {
-
-
-
-
   return (
     <div>
       <h1>Home section</h1>
-
+      <About />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
