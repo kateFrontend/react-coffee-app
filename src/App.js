5e6925185953dd38357806a9a547a6fa7ae5f0a2
kateFrontend <ecdanilova@gmail.com>
2022-12-09 17:28:00 -0500

    setup pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Types from "./pages/Types";
 import Benefits from "./pages/Benefits"
-import Contact from "./pages/Contact";
+import History from "./pages/History"
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
@@ -17,22 +17,22 @@ function App() {
         <Link to="/" className="link">
           Home
         </Link>
+        <Link to="/history" className="link">
+          History of coffee
+        </Link>
         <Link to="/types" className="link">
           Coffee types
         </Link>
         <Link to="/benefits" className="link">
           Coffee benefits
         </Link>
-        <Link to="/contact" className="link">
-          Contact Us
-        </Link>
       </nav> 
 
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/history" element={<History/>} />/
         <Route path="/types" element={<Types />} />
         <Route path="/benefits" element={<Benefits />} />
-        <Route path="/contact" element={<Contact />} />/
       </Routes>
     </Router>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import About from "../components/About";
+
 
 function Home() {
   return (
     <div>
       <h1>Home section</h1>
-      <About />
+
     </div>
   );
 }
