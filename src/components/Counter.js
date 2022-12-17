1900c2bd48b3f2942fc1f2df206b04026ea11ab8
kateFrontend <ecdanilova@gmail.com>
2022-12-17 12:18:54 -0500

    install dependencies

---
+++ src/components/Counter.js
@@ -0,0 +1,126 @@
+import { isVisible } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ReactVisibilitySensor from "react-visibility-sensor";
+
+function Counter({ className, ...rest }) {
+  const [viewPortEntered, setViewPortEntered] = useState(false);
+
+  return (
+    <>
+      <section className="counter">
+        <div className="title">
+          <h1>our achievements</h1>
+        </div>
+
+        <div className="counter-row">
+          <div className="counter-column">
+            <strong data-number="310">
+              <CountUp {...rest} start={viewPortEntered ? null : 0} end={310}>
+                {({ countUpRef }) => {
+                  return (
+                    <ReactVisibilitySensor
+                      active={!viewPortEntered}
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setViewPortEntered(true);
+                        }
+                      }}
+                      delayedCall
+                    >
+                      <span className="number" ref={countUpRef} />
+                    </ReactVisibilitySensor>
+                  );
+                }}
+              </CountUp>
+            </strong>
+            <span>
+              Some text <br />
+              Goes here
+            </span>
+          </div>
+
+          <div className="counter-column">
+            <strong data-number="1250">
+              <CountUp {...rest} start={viewPortEntered ? null : 0} end={1250}>
+                {({ countUpRef }) => {
+                  return (
+                    <ReactVisibilitySensor
+                      active={!viewPortEntered}
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setViewPortEntered(true);
+                        }
+                      }}
+                      delayedCall
+                    >
+                      <span className="number" ref={countUpRef} />
+                    </ReactVisibilitySensor>
+                  );
+                }}
+              </CountUp>
+            </strong>
+            <span>
+              Some text <br />
+              Goes here
+            </span>
+          </div>
+
+          <div className="counter-column">
+            <strong data-number="125">
+              <CountUp {...rest} start={viewPortEntered ? null : 0} end={125}>
+                {({ countUpRef }) => {
+                  return (
+                    <ReactVisibilitySensor
+                      active={!viewPortEntered}
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setViewPortEntered(true);
+                        }
+                      }}
+                      delayedCall
+                    >
+                      <span className="number" ref={countUpRef} />
+                    </ReactVisibilitySensor>
+                  );
+                }}
+              </CountUp>
+            </strong>
+            <span>
+              Some text <br />
+              Goes here
+            </span>
+          </div>
+
+          <div className="counter-column">
+            <strong data-number="24">
+              <CountUp {...rest} start={viewPortEntered ? null : 0} end={24}>
+                {({ countUpRef }) => {
+                  return (
+                    <ReactVisibilitySensor
+                      active={!viewPortEntered}
+                      onChange={(isVisible) => {
+                        if (isVisible) {
+                          setViewPortEntered(true);
+                        }
+                      }}
+                      delayedCall
+                    >
+                      <span className="number" ref={countUpRef} />
+                    </ReactVisibilitySensor>
+                  );
+                }}
+              </CountUp>
+            </strong>
+            <span>
+              Some text <br />
+              Goes here
+            </span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Counter;
