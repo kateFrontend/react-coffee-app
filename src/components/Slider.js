9cf05e70e7b94df947888e878b7ccb1c6fa24f35
kateFrontend <ecdanilova@gmail.com>
2022-12-17 20:06:21 -0500

    update slider css

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { sliderInfo } from '../data/sliderInfo'
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
+import '../css/Slider.css'
 
 function Slider() {
 const [review, setReview] = useState(0);
@@ -38,15 +39,17 @@ const nextQuote = () => {
             <FaQuoteRight className='quote-icon' />
             <h2 className='quote'>{quote}</h2>
             <p className='author'>{name}</p>
-        </div>
 
-        <button className='prev' onClick={previousQuote}>
+            <button className='prev' onClick={previousQuote}>
             <FiChevronLeft/>
         </button>
 
         <button className='next' onClick={nextQuote}>
             <FiChevronRight/>
         </button>
+        </div>
+
+
     </main>
   )
 }
