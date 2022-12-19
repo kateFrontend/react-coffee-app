79464d2d885d1f45af25dc126bd9513f819bded0
kateFrontend <ecdanilova@gmail.com>
2022-12-19 14:50:30 -0500

    video component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,57 +1,54 @@
-import React, { useState } from 'react'
-import { sliderInfo } from '../data/sliderInfo'
+import React, { useState } from "react";
+import { sliderInfo } from "../data/sliderInfo";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
-import '../css/Slider.css'
+import "../css/Slider.css";
 
 function Slider() {
-const [review, setReview] = useState(0);
-const { id, name, quote } = sliderInfo[review];
+  const [review, setReview] = useState(0);
+  const { id, name, quote } = sliderInfo[review];
 
-const previousQuote = () => {
+  const previousQuote = () => {
     setReview((review) => {
-        review--;
-        if (review < 0) {
-            return sliderInfo.length -1;
-        }
-        return review;
-    })
-}
+      review--;
+      if (review < 0) {
+        return sliderInfo.length - 1;
+      }
+      return review;
+    });
+  };
 
-const nextQuote = () => {
+  const nextQuote = () => {
     setReview((review) => {
-        review++;
-        if (review > sliderInfo.length -1) {
-            review = 0;
-        }
-        return review;
-    })
-}
+      review++;
+      if (review > sliderInfo.length - 1) {
+        review = 0;
+      }
+      return review;
+    });
+  };
 
   return (
     <main>
-        <div className='slider-title'>
-            <h1>Coffee quotes for coffee lovers</h1>
-        </div>
+      <div className="slider-title">
+        <h1>Coffee quotes for coffee lovers</h1>
+      </div>
 
-        <div className='slider'>
+      <div className="slider">
+        <FaQuoteRight className="quote-icon" />
+        <h2 className="quote">{quote}</h2>
+        <p className="author">{name}</p>
 
-            <FaQuoteRight className='quote-icon' />
-            <h2 className='quote'>{quote}</h2>
-            <p className='author'>{name}</p>
-
-            <button className='prev' onClick={previousQuote}>
-            <FiChevronLeft/>
+        <button className="prev" onClick={previousQuote}>
+          <FiChevronLeft />
         </button>
 
-        <button className='next' onClick={nextQuote}>
-            <FiChevronRight/>
+        <button className="next" onClick={nextQuote}>
+          <FiChevronRight />
         </button>
-        </div>
-
-
+      </div>
     </main>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
---
+++ src/components/Video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import video from '../assets/video.mp4'
+
+function Video() {
+  return (
+    <>
+    <video autoPlay muted loop>
+        <source src={video} type='video/mp4'/>
+    </video>
+    </>
+  )
+}
+
+export default Video
