978b17d19c91ca5b5233a31ef76cd08d7d7383c6
kateFrontend <ecdanilova@gmail.com>
2022-12-20 16:36:14 -0500

    update css

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
     name: "Stimulates energy",
     text: "Everyone is familiar with the way a cup or two of coffee stimulates our energy with its caffeine content. It is invaluable to anyone in a stressful situation who struggles to complete a task when natural energy has drained. However, in common with so many other substances you can have too much of a good thing. There is a huge difference between taking coffee as an occasional energy stimulant and becoming too reliant on the drink. Certain groups also need to be careful about their coffee intake. For example, the UK’s National Health Service advises pregnant women to restrict their caffeine intake to 200mg per day.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1507226983735-a838615193b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     showMore: false,
   },
   {
@@ -12,7 +12,7 @@ export const data = [
     name: "High antioxidant qualities",
     text: "The human body is involved in a constant struggle to resist toxic substances absorbed through food and drinks, pollution in the environment, drugs and other ways. Coffee possesses valuable antioxidant properties. This means that when you drink that cup of coffee, as well as boosting your energy, you strengthen you help clean out those toxins from your body. Drinking coffee, therefore, reduces the risk of infections developing and other illnesses taking hold. However, this does not imply that someone with an infection can expect to cure it with a few cups alone. Once you have the infection, you need to see a doctor.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1637&q=80",
     showMore: false,
   },
   {
@@ -20,7 +20,7 @@ export const data = [
     name: "A key to longevity?",
     text: "One of the fascinating claims made about the value of coffee drinking is that it can extend your lifespan. A number of academic studies suggest that regular coffee drinkers live longer than those who do not drink any. These findings are strongly disputed in scientific circles. Some claim that these researchers did not take sufficient account of the different social and economic differences that influence longevity. However, if coffee drinking reduces the risk of disease people will consequently live longer.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1439242088854-0c76045f4124?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     showMore: false,
   },
   {
@@ -28,7 +28,7 @@ export const data = [
     name: "Keep thin by drinking coffee",
     text: "If you are one of the multitudes of people struggling with a weight problem you are going to be delighted to know that coffee drinking might actually help you to slim. There is a certain amount of evidence that coffee causes an increase in metabolic rates and this helps burn up excess body fat. There is serious interest in how caffeine could play a role in obesity treatments, but the research findings are not yet conclusive.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1498653893439-4a6b2ed3b9ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     showMore: false,
   },
   {
@@ -36,7 +36,7 @@ export const data = [
     name: "Reduce the risk of developing diabetes",
     text: "The rise in the number of diabetics across the western world raises major health concerns. There are hopes that the medicinal properties of coffee may lower the risk of developing type 2 diabetes. Based on the findings of University of Harvard Dr. Hu, it appears there is a clear correlation between the amount of coffee drunk and lower type 2 diabetes risks. He found that each cup drank reduced this risk by 9%. While this evidence is encouraging people still need to exercise and watch their diet to improve their chances of avoiding this disease.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1623593721974-f39b78528626?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     showMore: false,
   },
   {
@@ -44,7 +44,7 @@ export const data = [
     name: "Protection against Alzheimer’s",
     text: "This degenerative, debilitating disease appears to be increasing in frequency although this is certainly in part due to more people reaching advanced ages. Research is ongoing, but many gaps in scientific understanding remain unplugged. Whether or not coffee drinkers have less chance of developing Alzheimer’s disease is one of the issues under investigation. Current findings lend some support to claims coffee drinking might lower the risk, but much more research is needed to draw definite conclusions.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
     showMore: false,
   },
   {
@@ -52,7 +52,7 @@ export const data = [
     name: "Who needs performance-boosting drugs?",
     text: "Anyone who follows the athletics news has heard about the recent scandals involving illegal drugs. A number of international athletes have been disqualified for using performance-boosting drugs to help them win competitions. If only they knew about an alternative but legal way to improve their track performance. Certain researchers claim that if athletes drink coffee before they exercise this stimulus booster can improve their abilities by at least ten percent. These researchers argue that coffee leads to higher oxygen consumption while exercising and it encourages the body to break up fat into energy.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1475296204602-08d15839e95f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     showMore: false,
   },
   {
@@ -60,7 +60,7 @@ export const data = [
     name: "A boost to mental health",
     text: "In addition to creating physical energy, is it possible that coffee drinking also contributes to emotional wellbeing? Its role as a relaxant is popular knowledge, but some studies suggest it goes much further than this. Research done by the Harvard School of Public Health indicates that women who drank four or more cups per day reduce their risk of developing depression by a fifth. Another study suggested that regular coffee drinkers are less likely to become suicidal.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1514163161321-f4f7c2a90296?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     showMore: false,
   },
   {
@@ -68,15 +68,15 @@ export const data = [
     name: "Decreases risks of developing heart disease",
     text: "Heart disease is now a major cause of death in developed countries. Anything that could reduce the risks of developing this disease is certain to receive an enthusiastic welcome. The humble cup of coffee might be just what is needed according to a study made in Korea. These researchers found that three cups per day is enough to lower the chances of developing heart disease. European and American scientists point out how dietary differences between their countries and Korea raise questions about the applicability of this research in their societies.",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1507915135761-41a0a222c709?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
     showMore: false,
   },
   {
     id: 10,
-    name: "Decreases risks of developing heart disease",
-    text: "Heart disease is now a major cause of death in developed countries. Anything that could reduce the risks of developing this disease is certain to receive an enthusiastic welcome. The humble cup of coffee might be just what is needed according to a study made in Korea. These researchers found that three cups per day is enough to lower the chances of developing heart disease. European and American scientists point out how dietary differences between their countries and Korea raise questions about the applicability of this research in their societies.",
+    name: "Helps prevent Parkinson's disease",
+    text: "According to research carried out in Sweden, it seems that coffee drinkers are less likely to get this debilitating disease even if their genetic makeup makes them more prone to it. Additional evidence also shows that people with Parkinson's disease are less likely to be coffee drinkers than their healthy brothers and sister, but it also indicates they are less likely to smoke cigarettes!",
     image:
-      "https://images.unsplash.com/photo-1583485138683-28fefff0f728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      "https://images.unsplash.com/photo-1510083300914-5ffac797d9d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     showMore: false,
   },
 ];
--- src/pages/Benefits.js
+++ src/pages/Benefits.js
@@ -1,31 +1,33 @@
-import {useState} from "react";
+import { useState } from "react";
 
-import { data } from "../data/data"
+import { data } from "../data/data";
 
 function Benefits() {
   const [benefits, setBenefits] = useState(data);
   const [showMore, setShowMore] = useState(false);
 
   const removeBenefit = (id) => {
-    let newBenefit = benefits.filter((element) => element.id !==id);
-    setBenefits(newBenefit)
-  }
+    let newBenefit = benefits.filter((element) => element.id !== id);
+    setBenefits(newBenefit);
+  };
 
   const showTextClick = (element) => {
-    element.showMore = !element.showMore
-    setShowMore(!showMore)
-  }
+    element.showMore = !element.showMore;
+    setShowMore(!showMore);
+  };
 
-if(benefits.length === 0) {
+  if (benefits.length === 0) {
     return (
-        <main>
-            <div>
-                <h2>no benefits left</h2>
-                <button className="btn" onClick={() => window.location.reload(false)}>Click to reload benefits</button>
-            </div>
-        </main>
-    )
-}
+      <>
+        <div className="reload">
+          <h2>No benefits left</h2>
+          <button className="btn-reload" onClick={() => window.location.reload(false)}>
+            Click to reload benefits
+          </button>
+        </div>
+      </>
+    );
+  }
 
   return (
     <section>
@@ -34,23 +36,45 @@ if(benefits.length === 0) {
       </div>
 
       <div className="benefit-main">
-       {benefits.map((element) => {
-        const { id, name, text, image, showMore } = element;
-        return (
+        {benefits.map((element) => {
+          const { id, name, text, image, showMore } = element;
+          return (
             <article className="one-benefit" key={id}>
-                <img src={image} alt={name} />
-                <footer>
-                    <div className="description">
-                        <h2 className="subtitle">{name}</h2>
-                        <p className="benefit-info">{showMore ? text : text.substring(0,180) + "....."}
-                        <button className="btn" onClick={() => showTextClick(element)}>{showMore ? "show less" : "show more"}</button></p>
-                        <button className="delete-btn" onClick={() => removeBenefit(id)}>not interested</button>
-                    </div>
-                </footer>
+              <img src={image} alt={name} />
+              <footer>
+                <div className="description">
+                  <h2 className="subtitle">{name}</h2>
+                  <p className="benefit-info">
+                    {showMore ? text : text.substring(0, 180) + "....."}
+                    <button
+                      className="btn"
+                      onClick={() => showTextClick(element)}
+                    >
+                      {showMore ? "show less" : "show more"}
+                    </button>
+                  </p>
+                  <button
+                    className="delete-btn"
+                    onClick={() => removeBenefit(id)}
+                  >
+                    not interested
+                  </button>
+                </div>
+              </footer>
             </article>
-        )
-       })}
+          );
+        })}
       </div>
+      <p className="info center">
+        Information from the
+        <a
+          className="link"
+          href="https://facty.com/food/nutrition/10-health-benefits-of-coffee/"
+        >
+          source
+        </a>
+        for educational purposes only
+      </p>
     </section>
   );
 }
