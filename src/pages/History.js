b13f8a0365741d47d23f01874e4b53b7476f7fff
kateFrontend <ecdanilova@gmail.com>
2022-12-20 10:34:45 -0500

    update sections

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,46 +1,145 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function History() {
   return (
-    <section className="section about-section">
-    <main>
-      <h1 className="about-title">History of coffee</h1>
-      <p className="par-about">
-        The history of coffee dates back to centuries of old oral tradition in
-        Africa. Coffee plants grew wild in Ethiopia and were widely used by
-        nomadic tribes for thousands of years. Sufi monasteries in Yemen
-        employed coffee as an aid to concentration during prayers. Roasting
-        the seeds wasn't a way to serve coffee until the 1400's. During the
-        cultivation, brewed coffee was reserved exclusively for the priesthood
-        and the medical profession; doctors would use the brew for patients
-        experiencing a need for better digestion, and priests used it to stay
-        alert during their long nights of studying for the church. Coffee
-        later spread to Europe in the early 16th century; it caused some
-        controversy on whether it was halal in Ottoman and Mamluk society.
-        Coffee arrived in Italy the second half of the 16th century through
-        commercial Mediterranean trade routes, only being served to the
-        wealthy. Central and Eastern Europeans learned of coffee from the
-        Ottomans. By the mid 17th century, it had reached India and the East
-        Indies. Coffee houses would establish themselves in Western Europe by
-        the late 17th century, especially in England and Germany. In many
-        cultures, if you could afford to serve coffee to your guests, it was a
-        sign of wealth and power. One of the earliest cultivations of coffee
-        in the New World was when Gabriel de Clieu brought coffee seedlings to
-        Martinique in 1720. These beans later sprouted 18,680 coffee trees
-        which enabled its spread to other Caribbean islands like
-        Saint-Domingue and also to Mexico. By 1788, Saint-Domingue supplied
-        half the world's coffee. By 1852, globally, Brazil became the largest
-        producer of coffee and has held that status ever since. The period
-        since 1950 saw the widening of the playing field due to the emergence
-        of several other major producers, notably Colombia, Ivory Coast,
-        Ethiopia, and Vietnam; the latter overtook Colombia and became the
-        second-largest producer in 1999. Modern production techniques along
-        with the mass productization of coffee has made it a household item
-        today.
-      </p>
-    </main>
-  </section>
-  )
+    <section className="about-section">
+      <>
+        <h1 className="about-title">The History of Coffee</h1>
+        <p className="italic">
+          No one knows exactly how or when coffee was discovered, though there
+          are many legends about its origin.
+        </p>
+
+        <h3>An Ethiopian Legend</h3>
+        <hr></hr>
+        <p className="par-about">
+          Coffee grown worldwide can trace its heritage back centuries to the
+          ancient coffee forests on the Ethiopian plateau. There, legend says
+          the goat herder Kaldi first discovered the potential of these beloved
+          beans. The story goes that that Kaldi discovered coffee after he
+          noticed that after eating the berries from a certain tree, his goats
+          became so energetic that they did not want to sleep at night. Kaldi
+          reported his findings to the abbot of the local monastery, who made a
+          drink with the berries and found that it kept him alert through the
+          long hours of evening prayer. The abbot shared his discovery with the
+          other monks at the monastery, and knowledge of the energizing berries
+          began to spread. As word moved east and coffee reached the Arabian
+          peninsula, it began a journey which would bring these beans across the
+          globe.
+        </p>
+
+        <h3>The Arabian Peninsula</h3>
+        <hr></hr>
+        <p className="par-about">
+          Coffee cultivation and trade began on the Arabian Peninsula. By the
+          15th century, coffee was being grown in the Yemeni district of Arabia
+          and by the 16th century it was known in Persia, Egypt, Syria, and
+          Turkey. Coffee was not only enjoyed in homes, but also in the many
+          public coffee houses — called qahveh khaneh — which began to appear in
+          cities across the Near East. The popularity of the coffee houses was
+          unequaled and people frequented them for all kinds of social activity.
+          Not only did the patrons drink coffee and engage in conversation, but
+          they also listened to music, watched performers, played chess and kept
+          current on the news. Coffee houses quickly became such an important
+          center for the exchange of information that they were often referred
+          to as “Schools of the Wise.” With thousands of pilgrims visiting the
+          holy city of Mecca each year from all over the world, knowledge of
+          this “wine of Araby” began to spread.
+        </p>
+
+        <h3>Coffee Comes to Europe</h3>
+        <hr></hr>
+        <p className="par-about">
+          European travelers to the Near East brought back stories of an unusual
+          dark black beverage. By the 17th century, coffee had made its way to
+          Europe and was becoming popular across the continent. Some people
+          reacted to this new beverage with suspicion or fear, calling it the
+          “bitter invention of Satan.” The local clergy condemned coffee when it
+          came to Venice in 1615. The controversy was so great that Pope Clement
+          VIII was asked to intervene. He decided to taste the beverage for
+          himself before making a decision, and found the drink so satisfying
+          that he gave it papal approval. Despite such controversy, coffee
+          houses were quickly becoming centers of social activity and
+          communication in the major cities of England, Austria, France, Germany
+          and Holland. In England “penny universities” sprang up, so called
+          because for the price of a penny one could purchase a cup of coffee
+          and engage in stimulating conversation. Coffee began to replace the
+          common breakfast drink beverages of the time — beer and wine. Those
+          who drank coffee instead of alcohol began the day alert and energized,
+          and not surprisingly, the quality of their work was greatly improved.
+          (We like to think of this a precursor to the modern office coffee
+          service.) By the mid-17th century, there were over 300 coffee houses
+          in London, many of which attracted like-minded patrons, including
+          merchants, shippers, brokers and artists. Many businesses grew out of
+          these specialized coffee houses. Lloyd's of London, for example, came
+          into existence at the Edward Lloyd's Coffee House.
+        </p>
+
+        <h3>The New World</h3>
+        <hr></hr>
+        <p className="par-about">
+          In the mid-1600's, coffee was brought to New Amsterdam, later called
+          New York by the British. Though coffee houses rapidly began to appear,
+          tea continued to be the favored drink in the New World until 1773,
+          when the colonists revolted against a heavy tax on tea imposed by King
+          George III. The revolt, known as the Boston Tea Party, would forever
+          change the American drinking preference to coffee. "Coffee - the
+          favorite drink of the civilized world." - Thomas Jefferson
+        </p>
+
+        <h3>Plantations Around the World</h3>
+        <hr></hr>
+        <p className="par-about">
+          As demand for the beverage continued to spread, there was fierce
+          competition to cultivate coffee outside of Arabia. The Dutch finally
+          got seedlings in the latter half of the 17th century. Their first
+          attempts to plant them in India failed, but they were successful with
+          their efforts in Batavia, on the island of Java in what is now
+          Indonesia. The plants thrived and soon the Dutch had a productive and
+          growing trade in coffee. They then expanded the cultivation of coffee
+          trees to the islands of Sumatra and Celebes.
+        </p>
+
+        <h3>Coming to the Americas</h3>
+        <hr></hr>
+        <p className="par-about">
+          In 1714, the Mayor of Amsterdam presented a gift of a young coffee
+          plant to King Louis XIV of France. The King ordered it to be planted
+          in the Royal Botanical Garden in Paris. In 1723, a young naval
+          officer, Gabriel de Clieu obtained a seedling from the King's plant.
+          Despite a challenging voyage — complete with horrendous weather, a
+          saboteur who tried to destroy the seedling, and a pirate attack — he
+          managed to transport it safely to Martinique. Once planted, the
+          seedling not only thrived, but it’s credited with the spread of over
+          18 million coffee trees on the island of Martinique in the next 50
+          years. Even more incredible is that this seedling was the parent of
+          all coffee trees throughout the Caribbean, South and Central America.
+          The famed Brazilian coffee owes its existence to Francisco de Mello
+          Palheta, who was sent by the emperor to French Guiana to get coffee
+          seedlings. The French were not willing to share, but the French
+          Governor's wife, captivated by his good looks, gave him a large
+          bouquet of flowers before he left— buried inside were enough coffee
+          seeds to begin what is today a billion-dollar industry. Missionaries
+          and travelers, traders and colonists continued to carry coffee seeds
+          to new lands, and coffee trees were planted worldwide. Plantations
+          were established in magnificent tropical forests and on rugged
+          mountain highlands. Some crops flourished, while others were
+          short-lived. New nations were established on coffee economies.
+          Fortunes were made and lost. By the end of the 18th century, coffee
+          had become one of the world's most profitable export crops. After
+          crude oil, coffee is the most sought commodity in the world.
+        </p>
+
+        <p className="info">
+          Information from the<a className="link" 
+          href="https://www.ncausa.org/about-coffee/history-of-coffee#:~:text=Coffee%20cultivation%20and%20trade%20began,Egypt%2C%20Syria%2C%20and%20Turkey.">
+            source 
+          </a>for educational purposes only
+        </p>
+      </>
+    </section>
+  );
 }
 
-export default History
+export default History;
