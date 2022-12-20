bb7e00577847f0f6b44e1cdc3166ca6d8ae755fa
kateFrontend <ecdanilova@gmail.com>
2022-12-20 09:35:14 -0500

    update sections

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -1,33 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../css/Sections.css'
+import "../css/Sections.css";
 
 function Sections() {
   return (
-    <div className="serv-section">
-      
+    <section className="serv-section">
       <h1 className="serv-title">Sections to explore</h1>
-      
+
       <section className="el-section">
         <Link to="/history">
-          <div className="element">
-            <h3 className="el-title">History <br/> of coffee</h3>
+          <div className="element one">
+            <h3 className="el-title">
+              History <br /> of coffee
+            </h3>
           </div>
         </Link>
 
         <Link to="/types">
-          <div className="element">
-            <h3 className="el-title">Coffee <br/> types</h3>
+          <div className="element two">
+            <h3 className="el-title">
+              Coffee <br /> types
+            </h3>
           </div>
         </Link>
 
         <Link to="/benefits">
-          <div className="element">
-            <h3 className="el-title">Coffee <br/> benefits</h3>
+          <div className="element three">
+            <h3 className="el-title">
+              Coffee <br /> benefits
+            </h3>
           </div>
         </Link>
       </section>
-    </div>
+    </section>
   );
 }
 
